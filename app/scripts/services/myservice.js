d5adead5e0f1f02fa2b93327ae786bafd6c8caad
Sean <seanleecode@gmail.com>
2016-03-31 11:20:22 +0800

    added services

---
+++ app/scripts/services/myservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name mytodoApp.myService
+ * @description
+ * # myService
+ * Service in the mytodoApp.
+ */
+angular.module('mytodoApp')
+  .service('myService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
